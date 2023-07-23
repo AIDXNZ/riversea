a9bfdaa73a5849c858fe5164294330f7c8085bca
Aidan M <asmaidan@outlook.com>
2023-07-23 16:12:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 echo 'deb http://download.opensuse.org/repositories/home:/RizinOrg/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/home:RizinOrg.list &&
 curl -fsSL https://download.opensuse.org/repositories/home:RizinOrg/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_RizinOrg.gpg > /dev/null &&
-sudo apt update;
+sudo apt update ;
 sudo apt install rizin
+
+git clone https://github.com/radareorg/radare2 ;
+cd radare2 ; sys/install.sh
